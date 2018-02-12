6dfe31d21656a7795d298e4d8d7f19c9a6355a49
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2018-02-12 21:16:13 +0100

    first commit

---
+++ R/CompoData.R
@@ -0,0 +1,10 @@
+
+##' The main message of this package
+##'
+##' The main message of this package is that it is simply a way to distribute a lecture
+##' @title CompoData
+##' @export
+##' @author Emanuel Heitlinger
+CompoData <- function(){
+    "This package is only for distribution of a lecture please use vignette()"
+}
